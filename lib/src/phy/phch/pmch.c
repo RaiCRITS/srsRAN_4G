201c3fd5cf05418a7522692d30798fa73ae7f5e3
5gmag-1 <alessandro.luccocastello@rai.it>
2025-03-12 11:31:27 +0100

    pmch CRITS mod

--- lib/src/phy/phch/pmch.c
+++ lib/src/phy/phch/pmch.c
@@ -19,6 +19,7 @@
  *
  */
 
+#include <complex.h> //added for ALC function
 #include <assert.h>
 #include <math.h>
 #include <stdbool.h>
@@ -333,7 +334,7 @@ int srsran_pmch_decode(srsran_pmch_t*         q,
                                     q->nof_rx_antennas,
                                     cfg->pdsch_cfg.grant.nof_re,
                                     1.0f,
-                                    channel->noise_estimate);
+                                    1.0f /*channel->noise_estimate*/);
 
     if (SRSRAN_VERBOSE_ISDEBUG()) {
       DEBUG("SAVED FILE subframe.dat: received subframe symbols");
@@ -354,6 +355,20 @@ int srsran_pmch_decode(srsran_pmch_t*         q,
      * thus we don't need tot set it in thde LLRs normalization
      */
     srsran_demod_soft_demodulate_s(cfg->pdsch_cfg.grant.tb[0].mod, q->d, q->e, cfg->pdsch_cfg.grant.nof_re);
+
+  // - modified ALC
+  
+    short *qb = q->e;
+    for (int i = 0; i < cfg->pdsch_cfg.grant.nof_re; i++) {
+      for (int j = 0; j < 4; j++){
+        float h;
+        //h = cabsf(q->ce[0][0][i])*0.05; //divide per 20 
+        h = (q->ce[0][0][i] * conj(q->ce[0][0][i]))*0.0025;
+        qb[i*4 + j] = qb[i*4 + j]*h;
+      }
+    }
+  
+  // - modified ALC
 
     /* descramble */
     srsran_scrambling_s_offset(&q->seqs[cfg->area_id]->seq[sf->tti % 10], q->e, 0, cfg->pdsch_cfg.grant.tb[0].nof_bits);
