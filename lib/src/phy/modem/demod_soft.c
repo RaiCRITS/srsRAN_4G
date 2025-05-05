ad548e2815991fa904b6bfde89a66d0d3a29035b
rubens <rubensbrraka00@gmail.com>
2025-05-05 14:18:23 +0200

    noise

--- lib/src/phy/modem/demod_soft.c
+++ lib/src/phy/modem/demod_soft.c
@@ -1076,6 +1076,7 @@ int srsran_demod_soft_demodulate_b(srsran_mod_t modulation, const cf_t* symbols,
     default:
       ERROR("Invalid modulation %d", modulation);
       return -1;
+
   }
   return 0;
 }
